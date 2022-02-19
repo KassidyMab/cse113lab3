a0f8e41f414756ded003557dfb25af014cf51625
KassidyMab <89809905+KassidyMab@users.noreply.github.com>
2022-02-18 17:40:21 -0700

    Update to array.c with more funcs

--- array.c
+++ array.c
@@ -75,9 +75,8 @@ float midpoint(int max, int min)
         return mid;
 }
 
-void get_count(int arr[], int leg[], int count)
+void get_count(int arr[], int leg[], int count, size_t size)
 {
-        size_t size = sizeof(arr)/sizeof(int);
         for (int i = 0; i < size; i++){
                 if (arr[i] < count){
                         leg[0]++;
@@ -136,5 +135,64 @@ void reverse(int arr[], size_t size)
 
         }
 
+
+}
+
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+  
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+  
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++){
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+  
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+
+int median(int arr[], size_t size)
+{
+	
+
+	return 0;
+}
+
+
+int odd_count(int arr[], int size)
+{
+	int total = 0;
+	for (int i = 0; i < size; i++){
+		if (arr[i]%2 == 1){
+			total++;
+		}
+	}
+	return total;
+
+}
+
+
+int even_count(int arr[], int size)
+{
+	int total = 0;
+	for (int i = 0; i < size; i++){
+		if (arr[i]%2 == 0){
+			total++;
+		}
+	}
+	return total;
 
 }
--- array.h
+++ array.h
@@ -24,6 +24,16 @@
 
 /* function declarations go here */
 void print_array(int a[], size_t size);
+int find_max(int arr[], size_t size);
+int find_min(int arr[], size_t size);
+float midpoint(int max, int min);
+void get_count(int arr[], int leg[], int count, size_t size);
+int linear_search(int arr[], int search, size_t size);
+int sum(int arr[], size_t size);
+void reverse(int arr[], size_t size);
+void selectionSort(int arr[], int n);
+void swap(int *xp, int *yp);
+
 
 #endif
 
